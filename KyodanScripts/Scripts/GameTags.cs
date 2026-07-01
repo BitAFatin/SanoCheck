0cfc357c91b6bd91775efa296fa0a16f8517bbe3
BitAFatin <a-fatin@urayama.ac.jp>
2026-07-01 05:45:34 +0000

    ꉞ

---
+++ KyodanScripts/Scripts/GameTags.cs
@@ -0,0 +1,9 @@
+public static class GameTags
+{
+    public const string Vase = "Vase";
+    public const string BrokenVase = "BrokenVase";
+    public const string Character = "Character";
+
+    public const string Warp = "Warp";
+
+}
